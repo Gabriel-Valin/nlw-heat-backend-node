678cacb92a7f51130a60573c8aec3d2453cd5717
Gabriel-Valin <gabrielvalincontato@gmail.com>
2021-10-18 23:45:50 -0300

    feat: validate github token and returning info from user

--- src/services/auth-user-service.ts
+++ src/services/auth-user-service.ts
@@ -1,11 +1,22 @@
 import axios from 'axios'
 import 'dotenv/config'
 
+interface IAccessTokenResponse {
+    access_token: string
+}
+
+interface IUserResponse {
+    avatar_url: string
+    login: string
+    id: number
+    name: string
+}
+
 export class AuthenticateUserService {
     async execute (code: string) {
         const url = `https://github.com/login/oauth/access_token`
 
-        const response = await axios.post(url, null, {
+        const { data: githubToken } = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_KEY,
                 client_secret: process.env.GITHUB_SECRET_KEY,
@@ -13,6 +24,12 @@ export class AuthenticateUserService {
             },
             headers: {
                 "Accept": "application/json"
+            }
+        })
+        
+        const response = await axios.get<IUserResponse>(`https://api.github.com/user`, {
+            headers: {
+                authorization: `Bearer ${githubToken.access_token}`
             }
         })
 
