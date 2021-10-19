14ec497b806fc78d15e915cc014af8f3d3080ee9
Gabriel-Valin <gabrielvalincontato@gmail.com>
2021-10-19 01:13:54 -0300

    implements message router and re-writing request type from express

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/auth-user-controllers'
+import { CreateMessageController } from './controllers/create-message-controller'
+import { ensureAuth } from './middleware/ensure-auth'
 
 export const routes = Router()
 
 routes.post('/auth', new AuthenticateUserController().handle)
+
+routes.post('/messages', ensureAuth, new CreateMessageController().handle)
